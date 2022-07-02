e83cb73299df95250c79365049209679569a3d49
Sarath Chandra Oruganti <39090092+SarathChandra24@users.noreply.github.com>
2022-07-02 18:50:20 -0400

    changed organization

--- list_org_repos.sh
+++ list_org_repos.sh
@@ -7,7 +7,6 @@ jq --version
 # organization="$1"
 # github_token="$2"
 # organization=kemistre
-ORG_NAME=kemistre
 github_api_url='https://api.github.com'
 
 
@@ -20,10 +19,10 @@ echo repository, html_url, api_url, created_at, pushed_at, advanced_security, se
 for (( c=$START; c<=$total_pages; c++ ))
 do
 	echo "=============Navigating Page number: $c============="
-	echo "=============Listing repos in organization: $ORG_NAME=============" 
-	list_repos_url="${github_api_url}/orgs/$ORG_NAME/repos?per_page=10&page=${c}"
+	echo "=============Listing repos in organization: $organization=============" 
+	list_repos_url="${github_api_url}/orgs/${organization}/repos?per_page=10&page=${c}"
 	echo "List Repo URL: $list_repos_url"
-    all_repo_list=$(curl --location --request GET -H 'Accept: application/vnd.github.+json' -H "Authorization: token $LOCAL_GITHUB_TOKEN" $list_repos_url)
+    all_repo_list=$(curl --location --request GET -H 'Accept: application/vnd.github.+json' -H "Authorization: token $github_token" $list_repos_url)
 	echo "${all_repo_list}" | jq -c ".[]" | while read repo;do
 		repo_name=$(echo $repo | jq '.name' | tr -d '"')
 		html_url=$(echo $repo | jq '.html_url' | tr -d '"')
@@ -31,7 +30,7 @@ do
 		created_at=$(echo $repo | jq '.created_at' | tr -d '"')
 		pushed_at=$(echo $repo | jq '.pushed_at' | tr -d '"')
 		# echo "${repo_name}, ${api_url}"
-        repo_details=$(curl --location --request GET -H 'Accept: application/vnd.github.+json' -H "Authorization: token $LOCAL_GITHUB_TOKEN" $api_url)
+        repo_details=$(curl --location --request GET -H 'Accept: application/vnd.github.+json' -H "Authorization: token $github_token" $api_url)
         advanced_security=$(echo $repo_details | jq '.security_and_analysis.advanced_security.status' | tr -d '"')
         secret_scanning=$(echo $repo_details | jq '.security_and_analysis.secret_scanning.status' | tr -d '"')
         secret_scanning_push_protection=$(echo $repo_details | jq '.security_and_analysis.secret_scanning_push_protection.status' | tr -d '"')
